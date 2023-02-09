9d90b41f6724975c6830b6d5fde1d74ad25aede1
Cody Lobdell <thecreator85@gmail.com>
2023-02-08 16:06:21 -0800

    saved cart to local storage

--- js/catalog.js
+++ js/catalog.js
@@ -25,7 +25,7 @@ function populateForm() {
 // When someone submits the form, we need to add the selected item to the cart
 // object, save the whole thing back to local storage and update the screen
 // so that it shows the # of items in the cart and a quick preview of the cart itself.
-function handleSubmit() {
+function handleSubmit(event) {
 
   // TODO: Prevent the page from reloading
   event.preventDefault();
